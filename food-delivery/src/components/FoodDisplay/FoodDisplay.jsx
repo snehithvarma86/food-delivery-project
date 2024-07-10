22f5fd7a0224dd868584e8c79d0064398ed45bb5
snehithvarma86 <55405536+snehithvarma86@users.noreply.github.com>
2024-07-10 18:37:02 +0530

    food-menu

--- food-delivery/src/components/FoodDisplay/FoodDisplay.jsx
+++ food-delivery/src/components/FoodDisplay/FoodDisplay.jsx
@@ -7,11 +7,11 @@ import React, { useContext } from 'react'
 const FoodDisplay = ({category}) => {
     const {food_list} =useContext(StoreContext)
   return (
-    <div className='food=display' id='food-display'>
+    <div className='food-display' id='food-display'>
         <h2>Top Dishes Near You</h2>
         <div className="food-display-list">
             {food_list.map((item,index)=>{
-                return <FoodItem key={index} id={item._id} name={item.name} description={item.description} image={item.image} />
+                return <FoodItem key={index} id={item._id} name={item.name} description={item.description} image={item.image} price={item.price}/>
             })}
         </div>
     </div>
--- food-delivery/src/components/FoodItem/FoodItem.jsx
+++ food-delivery/src/components/FoodItem/FoodItem.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import './FoodItem.css'
 import { assets } from '../../assets/assets'
+import { useContext } from 'react';
+import { StoreContext } from '../context/StoreContext';
 
 const FoodItem = ({id,name,price,description,image}) => {
+  
+  const {cartItems,addToCart,removeFromCart} =useContext(StoreContext);
+
   return (
     <div className='food-item'>
       <div className="food-item-img-contaner">
         <img src={image} alt="" className="food-item-img" />
+        {!cartItems[id]
+          ?<img className='add' onClick={()=>addToCart(id)} src={assets.add_icon_white} />
+          :<div className="food-item-counter">
+            <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt="" />
+            <p>{cartItems[id]}</p>
+            <img onClick={()=>addToCart(id)}  src={assets.add_icon_green}   alt="" />
+          </div>
+        }
       </div>
       <div className="food-item-info">
         <div className="food-item-name-rating">
@@ -14,8 +27,9 @@ const FoodItem = ({id,name,price,description,image}) => {
             <img src={assets.rating_starts} alt="" />
         </div>
         <p className="food-item-des">{description}</p>
-        <p className="food-item-price">{price}</p>
+        <p className="food-item-price">${price}</p>
       </div>
+
     </div>
   )
 }
