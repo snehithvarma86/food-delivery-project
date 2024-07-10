c5fcc8793f668906629fd6295382e1247288ef18
snehithvarma86 <55405536+snehithvarma86@users.noreply.github.com>
2024-07-10 23:38:16 +0530

    minor food display change

--- food-delivery/src/components/FoodDisplay/FoodDisplay.jsx
+++ food-delivery/src/components/FoodDisplay/FoodDisplay.jsx
@@ -11,7 +11,9 @@ const FoodDisplay = ({category}) => {
         <h2>Top Dishes Near You</h2>
         <div className="food-display-list">
             {food_list.map((item,index)=>{
+              if(category==='All' || category===item.category){
                 return <FoodItem key={index} id={item._id} name={item.name} description={item.description} image={item.image} price={item.price}/>
+              }  
             })}
         </div>
     </div>
